dbf99e318052573bb5e309b90087e9107674a56e
pjk <pjk@brighten.co.kr>
2023-04-21 18:00:16 +0900

    main grid 작업중

--- templates/pages/main/main.php
+++ templates/pages/main/main.php
@@ -2,6 +2,9 @@
 // include_once '../../include/common.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/echartsCustoms/templates/include/common.php';
 ?>
+<!-- echarts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/5.4.2/echarts.min.js"></script>
+
 <link rel="stylesheet" href="/echartsCustoms/assets/css/pages/main/layout.css">
 
 </head>
@@ -9,7 +12,7 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/echartsCustoms/templates/include/common
     <div class="wrap base_layout">
         <header>
             <nav>
-                
+
             </nav>
         </header>
         <aside class="leftSNB">
@@ -19,7 +22,14 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/echartsCustoms/templates/include/common
 
         </aside>
         <main>
-
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box">
+                <?php include_once $_SERVER['DOCUMENT_ROOT'].'/echartsCustoms/templates/include/charts/conductBookUseCount.php'; ?>
+            </div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
         </main>
         
     </div>
