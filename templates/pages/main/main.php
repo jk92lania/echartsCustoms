bb661459889980815ba86595ddd1e9270d70537f
pjk <pjk@brighten.co.kr>
2023-04-21 15:39:46 +0900

    scss 연결 test

--- templates/pages/main/main.php
+++ templates/pages/main/main.php
@@ -1,1 +1,19 @@
-main
+<?php
+
+// include_once '../../include/common.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/echartsCustoms/templates/include/common.php';
+
+?>
+<link rel="stylesheet" href="/echartsCustoms/assets/css/pages/main/layout.css">
+</head>
+<body>
+    <div class="wrap">
+        <div class="box">box</div>
+        <div class="box">box</div>
+        <div class="box">box</div>
+    </div>
+    <a href="">aaa</a>
+    
+    
+</body>
+</html>
